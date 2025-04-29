794f06b4faa04efc7d282b07e768c16f10c7c964
al-aminpatowary1038 <ariyanalamin1038@gmail.com>
2025-04-29 12:25:37 +0600

    update java Assignment

---
+++ BasicJava/Assignment1,Product/src/Product.java
@@ -0,0 +1,6 @@
+public class Product {
+    public static void main(String[] args) {
+        ProductList p1 = new ProductList();
+        p1.list();
+    }
+}
---
+++ BasicJava/Assignment1,Product/src/ProductList.java
@@ -0,0 +1,9 @@
+public class ProductList extends Product {
+    public void list() {
+        System.out.println("id: 101");
+        System.out.println("Titale:iphones");
+        System.out.println("Price: 1895 euros");
+        System.out.println("Description: perfect product with best image quality");
+        System.out.println("Category: phone");
+    }
+}
